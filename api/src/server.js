6958de6be85c249660a8905b1839dbee276b2bda
williamjs95 <williamj.silva@live.com>
2023-11-22 05:06:05 -0300

    expresse created

--- api/src/server.js
+++ api/src/server.js
@@ -1,6 +1,10 @@
+// chama express
 const express = require('express');
 
+// executa express
 const app = express();
 
+// declara porta
 const PORT = 3333;
+// app escuta porta
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
